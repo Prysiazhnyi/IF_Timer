3667bf484d6bd93572ee7edaac4013e7a29410dc
sprysiazhnyi <sprysiazhnyi@competo.io>
2025-02-25 22:25:55 +0200

    add

--- IF_Timer/ProfileViewController.swift
+++ IF_Timer/ProfileViewController.swift
@@ -11,6 +11,14 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var viewTab: UIView!
     
+    @IBOutlet weak var achievementsView: UIView!
+    @IBOutlet weak var fastingTrackerView: UIView!
+    @IBOutlet weak var statisticsFastingView: UIView!
+    @IBOutlet weak var weightView: UIView!
+    @IBOutlet weak var imtView: UIView!
+    
+    @IBOutlet weak var settingsButton: UIButton!
+    
     let backgroundTab = UIColor(red: 230/255, green: 245/255, blue: 255/255, alpha: 1)
     
     override func viewDidLoad() {
@@ -43,8 +51,45 @@ class ProfileViewController: UIViewController {
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
+        setupView()
+        setupSettingsButton()
     }
     
+    func setupView() {
+        let arrayViews: [UIView] = [achievementsView, fastingTrackerView, statisticsFastingView, weightView, imtView]
+        for view in arrayViews {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            // Настройка контейнера с информацией
+            view.backgroundColor = UIColor.white
+            view.layer.cornerRadius = 25
+            view.layer.shadowColor = UIColor.black.cgColor
+            view.layer.shadowOpacity = 0.1
+            view.layer.shadowOffset = CGSize(width: 0, height: 4)
+            view.layer.shadowRadius = 8
+            view.layer.masksToBounds = false
+            view.layer.masksToBounds = true
+        }
+    }
+    
+    func setupSettingsButton() {
+        settingsButton.backgroundColor = UIColor.white
+        settingsButton.layer.cornerRadius = 15
+        settingsButton.layer.masksToBounds = true
+        settingsButton.setTitleColor(.darkGray, for: .normal)
+        settingsButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        
+        // Устанавливаем изображение шестеренки перед текстом
+        let gearImage = UIImage(systemName: "gearshape.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.darkGray)
+        settingsButton.setImage(gearImage, for: .normal)
+        
+        // Устанавливаем текст кнопки
+        settingsButton.setTitle(NSLocalizedString("   Налаштування", comment: ""), for: .normal)
+        
+        // Настроим отступы между изображением и текстом
+        settingsButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)  // отступ для изображения
+        settingsButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)  // отступ для текста
+        
+    }
     
     
     
